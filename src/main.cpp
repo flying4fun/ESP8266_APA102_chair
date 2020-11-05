89211038c97140417b489577c5baaf6aa7424ab8
Vernon McPherron <vernon@house.typeset.net>
2020-11-04 17:48:34 -0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -707,7 +707,7 @@ void cylon() {
     }
   }
   */
-  EVERY_N_MILLISECONDS(speed) {
+  EVERY_N_MILLISECONDS(speed/2) {
 
     if( bounceA ) {
       locationA++;
@@ -1330,27 +1330,30 @@ void Fire2012WithPalette() {
   // Array of temperature readings at each simulation cell
   static byte heat[NUM_LEDS/2];
 
-  // Step 1.  Cool down every cell a little
-  for( int i = 0; i < NUM_LEDS; i++) {
-    heat[i] = qsub8( heat[i],  random8(0, ((COOLING * 10) / (NUM_LEDS/2)) + 2));
-  }
+  black();
+  EVERY_N_MILLISECONDS(speed) {
+    // Step 1.  Cool down every cell a little
+    for( int i = 0; i < NUM_LEDS; i++) {
+      heat[i] = qsub8( heat[i],  random8(0, ((COOLING * 10) / (NUM_LEDS/2)) + 2));
+    }
 
-  // Step 2.  Heat from each cell drifts 'up' and diffuses a little
-  for( int k= (NUM_LEDS/2) - 3; k > 0; k--) {
-    heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
-  }
+    // Step 2.  Heat from each cell drifts 'up' and diffuses a little
+    for( int k= (NUM_LEDS/2) - 3; k > 0; k--) {
+      heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
+    }
 
-  // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
-  if( random8() < SPARKING ) {
-    int y = random8(7);
-    heat[y] = qadd8( heat[y], random8(160,255) );
-  }
+    // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
+    if( random8() < SPARKING ) {
+      int y = random8(7);
+      heat[y] = qadd8( heat[y], random8(160,255) );
+    }
 
-  // Step 4.  Map from heat cells to LED colors
-  for( int j = 0; j < (NUM_LEDS/2); j++) {
-    // Scale the heat value from 0-255 down to 0-240
-    // for best results with color palettes.
-    byte colorindex = scale8( heat[j], 240);
-    leds[j] = ColorFromPalette( gPal, colorindex);
+    // Step 4.  Map from heat cells to LED colors
+    for( int j = 0; j < (NUM_LEDS/2); j++) {
+      // Scale the heat value from 0-255 down to 0-240
+      // for best results with color palettes.
+      byte colorindex = scale8( heat[j], 240);
+      leds[j] = ColorFromPalette( gPal, colorindex);
+    }
   }
 }
